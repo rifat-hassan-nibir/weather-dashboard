4d210bcbc772cce7dc470c45d383f45f204616ae
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-08-28 08:43:15 +0600

    feat: add WeatherContext and WeatherProvider for weather data management

---
+++ src/provider/WeatherProvider.jsx
@@ -0,0 +1,10 @@
+import { WeatherContext } from "../context";
+import { useWeather } from "../hooks";
+
+const WeatherProvider = ({ children }) => {
+  const { weatherData, loading, error } = useWeather();
+
+  return <WeatherContext.Provider value={{ weatherData, loading, error }}>{children}</WeatherContext.Provider>;
+};
+
+export default WeatherProvider;
