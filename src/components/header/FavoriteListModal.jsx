99be845ee6bfa236818f03cb08a31c2e29a0b650
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2025-08-30 09:07:43 +0600

    feat: integrate location context into FavoriteListModal for location selection

--- src/components/header/FavoriteListModal.jsx
+++ src/components/header/FavoriteListModal.jsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
-import { FavoriteContext } from "../../context";
+import { FavoriteContext, LocationContext } from "../../context";
 
 export default function FavoriteListModal() {
   const { favorites } = useContext(FavoriteContext);
+  const { setSelectedLocation } = useContext(LocationContext);
 
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
@@ -10,7 +11,7 @@ export default function FavoriteListModal() {
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
         {favorites.length > 0 ? (
           favorites.map((fav) => (
-            <li key={fav.location} className="hover:bg-gray-200">
+            <li onClick={() => setSelectedLocation(fav)} key={fav.location} className="hover:bg-gray-200">
               {fav.location}
             </li>
           ))
